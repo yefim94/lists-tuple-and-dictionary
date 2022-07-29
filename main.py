35b50f68cb137859b149740171bb555cfcfdbec7
Yefim Blokh <73265655+yefim94@users.noreply.github.com>
2022-07-29 01:49:49 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+#lists
+ex_List = ["0","1","2"]
+def sum_list(items):
+    sum_numbers = 0
+    for x in items:
+        sum_numbers += x
+    return sum_numbers
+print(sum_list([1,2,-8]))
+#Dictionary
+dic1={1:10, 2:20}
+dic2={3:30, 4:40}
+dic3={5:50,6:60}
+dic4 = {}
+for d in (dic1, dic2, dic3): dic4.update(d)
+print(dic4)
+#tuple
+tup = ('e', 'x', 'e', 'r', 'c', 'i', 's', 'e', 's')
+str =  ''.join(tup)
+print(str)
